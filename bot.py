d35a0edf62df7ed9554ae6e76b93668655345b10
YAMOVIES <ya753121988@gmail.com>
2026-05-24 19:45:28 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ API_TOKEN = '8501387772:AAH8dn31CMywDrF0nSjM7TMfB2uA8i-Nfzg'
 MONGO_URI = 'mongodb+srv://drama:drama@cluster0.sa4kvgu.mongodb.net/DramaStoreDB?retryWrites=true&w=majority&appName=Cluster0'
 ADMIN_ID = 8932594210
 # আপনার ভার্সেল অ্যাপের ফুল লিঙ্ক (যেমন: https://mybot.vercel.app)
-WEB_URL = "https://your-app-name.vercel.app" 
+WEB_URL = "https://filestore-jet.vercel.app" 
 
 bot = telebot.TeleBot(API_TOKEN, threaded=False)
 app = Flask(__name__)
